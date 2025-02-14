e4f4c7cd441a1466eb6c99f6d1e5840e4315749b
Petr Breit <104927244+PanUcitelCz@users.noreply.github.com>
2025-02-14 11:29:51 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $name = "Petr"; // $ - Pravý alt + ů
+        $text = "Uživatel";
+    ?>
+    <h1>Náš první projekt v PHP</h1>
+
+    <p>Zde je jméno účastníka zájezdu: <?php echo "$name Breit";?>
+    </p>
+
+    <?php echo "<h3> Proměnná: $name </h3>" ?>
+
+    <?php 
+        $i = 1;
+
+        while($i <= 6){
+            echo "<h3> $text $i </h3>";
+            $i++;
+        }
+
+        for ($j = 0; $j <= 10; $j++) {
+            echo "The number is: $j <br>";
+          }
+    ?>
+
+</body>
+</html>
