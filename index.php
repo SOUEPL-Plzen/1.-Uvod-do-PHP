de123e29fd5f3b42739efc7b02fba24ed929f6e8
Petr Breit <104927244+PanUcitelCz@users.noreply.github.com>
2025-03-17 09:18:58 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -6,7 +6,64 @@
     <title>Document</title>
 </head>
 <body>
-    <?php include "components/header.php"?>
-    <h1>funguje</h1>
+    <?php include "header.php" ?>
+    
+    <main>
+        <h1>Zde je content</h1>
+
+        <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "root";
+            $dbname = "priprava_test";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "SELECT * FROM žák";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+            // output data of each row
+
+                echo "<table>
+                        <thead>
+                            <tr>
+                                <th>id</th>
+                                <th>Jméno</th>
+                                <th>Příjmení</th>
+                                <th>Třída</th>
+                                <th>Věk</th>
+                            </tr>
+                        </thead>
+                        <tbody>";
+
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>".
+                        "<td>" . $row["id"] . "</td>" .
+                        "<td>" . $row["Jméno"] . "</td>" .
+                        "<td>" . $row["Příjmení"] . "</td>" .
+                        "<td>" . $row["Třída"] . "</td>" .
+                        "<td>" . $row["Věk"] . "</td>" .
+                    "<tr>";
+                }
+                echo "</tbody>
+                        </table>";
+            } else {
+            echo "0 results";
+            }
+            $conn->close();
+        ?>
+    </main>
+
+    <style>
+        table, tr, td, th, thead{
+            border: 1px solid black
+        }
+    </style>
 </body>
 </html>
